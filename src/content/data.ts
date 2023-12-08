8a0ff912e975d60291a5f2050a84a96b963710e4
theoyoth <theobantang1@gmail.com>
2023-12-08 13:10:55 +0800

    first commit

---
+++ src/content/data.ts
@@ -0,0 +1,72 @@
+import earthIcon from "../../public/img/Earth.png"
+import igIcon from "../../public/img/instagram.png"
+import youtubeIcon from "../../public/img/youtube.png"
+import pinterestIcon from "../../public/img/pinterest.png"
+import githubIcon from "../../public/img/github.png"
+import devianIcon from "../../public/img/devianart.png"
+
+
+const Data = [
+  {
+    link:'https://theoyoth.vercel.app/',
+    icon:earthIcon,
+    title:'My website',
+    p:'include my portfolio and my projects'
+  },
+  {
+    link:'https://www.instagram.com/theoyoth/?hl=id',
+    icon:igIcon,
+    title:'Personal IG',
+    p:'social life'
+  },
+  {
+    link:'https://www.instagram.com/theoyothart/?hl=id',
+    icon:igIcon,
+    title:'Design IG',
+    p:'include my photo manipulation and poster design'
+  },
+  {
+    link:'https://www.instagram.com/theoyothtyping/?hl=id',
+    icon:igIcon,
+    title:'Type IG',
+    p:'my typography and graphics design'
+  },
+  {
+    link:'https://www.youtube.com/@theoyoth',
+    icon:youtubeIcon,
+    title:'theoyoth',
+    p:'my youtube channel where I uploaded my design process'
+  },
+  {
+    link:'https://www.youtube.com/@codebytheo',
+    icon:youtubeIcon,
+    title:'codebytheo',
+    p:'my channel where I code for something'
+  },
+  {
+    link:'https://id.pinterest.com/theoyothart/_created/',
+    icon:pinterestIcon,
+    title:'theoyothart',
+    p:'I uploaded my design here'
+  },
+  {
+    link:'https://github.com/theoyoth',
+    icon:githubIcon,
+    title:'theoyoth',
+    p:'my repo include all my projects'
+  },
+  {
+    link:'https://github.com/codebytheo',
+    icon:githubIcon,
+    title:'codebytheo',
+    p:'my repo from coding youtube'
+  },
+  {
+    link:'https://www.deviantart.com/yoth1/gallery',
+    icon:devianIcon,
+    title:'devianArt',
+    p:'include my photos manipulation'
+  },
+]
+
+export default Data
